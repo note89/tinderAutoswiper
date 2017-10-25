7a9cc81c755a1d25ebf064daa91f64a66f580bae
Nils Eriksson <Nilseriksson89.ne@gmail.com>
2017-10-25 21:16:27 +0200

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("hello from google");
+const element =  document.querySelector('button[aria-label="Like"]')
+const element1 = document.getElementsByClassName("recsGamepad__button--like")[0]
